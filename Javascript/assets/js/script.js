12b7a087fcb083e5759be6598359f781cb3c00e7
Amilton Oliveira <amilton.mac@gmail.com>
2022-08-10 22:58:32 -0300

    Estudo de JavaScript na Dio

---
+++ Javascript/assets/js/script.js
@@ -0,0 +1,32 @@
+var currentNumberWrapper=document.getElementById('currentNumber');
+var currentNumber = 0;
+
+
+function increment(){
+    currentNumber=currentNumber+1;
+    currentNumber.innerHTML = currentNumber+1;
+}
+
+function decrement(){
+    currentNumber=currentNumber+1;
+    currentNumber.innerHTML = currentNumber+1;
+}
+
+
+let count = 0;
+
+const CURRENT_NUMBER = document.getElementById('currentNumber');
+
+function increment() {
+	count++;
+	CURRENT_NUMBER.innerHTML = count;
+}
+
+function decrement() {
+	count--;
+	CURRENT_NUMBER.innerHTML = count;
+}
+
+function test() {
+	kdowkdpo;
+}
